bd557f5e860495f799250abfa91853736f91be00
Alice Mastrilli <alice.mastrilli@studio.unibo.it>
2021-06-20 14:56:37 +0200

    Added Timer class

---
+++ Timer.py
@@ -0,0 +1,31 @@
+from time import sleep
+
+
+class Timer:
+    __isTimerOver = False
+
+    def __init__(self, __duration: int):
+        self.__duration = __duration
+
+    def start_timer(self):
+        while self.duration > 0 and not bool(self.__isTimerOver):
+            # print(self.duration)
+            self.__duration = self.__duration - 1
+            sleep(1)
+        if self.__duration == 0:
+            self.__isTimerOver = True
+
+    def stop_timer(self):
+        self.__isTimerOver = True
+
+    def get_remaining_time(self):
+        return self.__duration
+
+    def is_timer_over(self):
+        return self.__isTimerOver
+
+# PROVA
+# timer = Timer(5)
+# print(Timer.is_timer_over(timer))
+# Timer.start_timer(timer)
+# print(Timer.is_timer_over(timer))
