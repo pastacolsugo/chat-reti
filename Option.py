ebed91f55cc378204886527f787504e9c57cb0f5
Alice Conti <conti.alice14@gmail.com>
2021-06-20 23:18:28 +0200

    Finish the Option class

--- Option.py
+++ Option.py
@@ -1,10 +1,19 @@
 import string
 
 class Option:
-    def __init__(self, option: string, isCorrect: int):
+    def __init__(self, option: string, isCorrect: bool):
         self.__option = option
+        self.__isCorrect = isCorrect
 
     def getOption(self):
-        
+        return self.__option
 
+    def isCorrect(self):
+        return self.__isCorrect
+
+
+#PROVA
+#option = Option("aliceisbeautiful", True)
+#print(Option.getOption(option))
+#print(Option.isCorrect(option))
 
