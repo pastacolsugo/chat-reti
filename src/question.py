72a6361350b5de2509e126d1b8f0813426151c61
Ugo <ugobaroncini@gmail.com>
2021-06-21 20:14:44 +0200

    Refactored more variable and method names.

--- src/question.py
+++ src/question.py
@@ -1,16 +1,16 @@
 class Question:
-    def __init__(self, question: str, correctAnswer: int, answers):
+    def __init__(self, question: str, correct_answer: int, answers: list[str]):
         self._question = question
-        self._correctAnswer = correctAnswer
+        self._correct_answer = correct_answer
         self._answers = answers
     
-    def getQuestion(self):
+    def get_question(self):
         return self._question
     
-    def getAnswers(self):
+    def get_answers(self):
         return self._answers
 
-    def getCorrectAnswer(self):
-        return self._correctAnswer
+    def get_correct_answer(self):
+        return self._correct_answer
 
         
