0a0276bbb1f3d8f8ed8f31ac77646a7401f05070
Ugo <ugobaroncini@gmail.com>
2021-06-23 17:12:17 +0200

    Started debugging

--- src/controller.py
+++ src/controller.py
@@ -1,13 +1,13 @@
 from time import sleep
 import threading
-from game import Game
 import socket
 import json
 import random
 
 HOST_ADDR = '127.0.0.1'
-HOST_PORT = '8080'
-CONFIG_FILE_PATH = 'config.json'
+HOST_PORT = 8080
+# CONFIG_FILE_PATH = './config.json'
+CONFIG_FILE_PATH = '/Users/sugo/git/chat-reti/src/config.json'
 with open(CONFIG_FILE_PATH, 'r') as config_file:
     GAME_CONFIG = json.load(config_file)
 
@@ -15,15 +15,19 @@ server_socket = None
 client_sockets = {}
 next_connection_id = 0
 has_game_started = False
+is_game_over = False
+ranking = {}
 
-def open_socket(self):
+cancer_flag = False
+
+def open_socket():
     global server_socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     server_socket.bind((HOST_ADDR, HOST_PORT))
     server_socket.listen(5) # allowed connection attempts
 
-    threading._start_new_thread(accept_new_clients, (server_socket))
+    threading._start_new_thread(accept_new_clients, (server_socket,))
 
 def accept_new_clients(server_socket):
     global client_sockets
@@ -42,7 +46,7 @@ def accept_new_clients(server_socket):
         }
 
         # spawna un nuovo thread e gli da in gestione quella connessione (tramite ConnectionID)
-        threading._start_new_thread(handle_player_connection, (id))
+        threading._start_new_thread(handle_player_connection, (id,))
 
 def get_next_connection_id():
     global next_connection_id
@@ -51,55 +55,104 @@ def get_next_connection_id():
     return id
 
 def handle_player_connection(id):
+    global is_game_over, GAME_CONFIG, ranking
+    score = 0
     player_socket = client_sockets[id]['socket']
 
     # read name
     player_name = player_socket.recv(4096)
+    if not player_name:
+        print ('no player name')
+        return
 
     # assign role
     player_role = random.choice(GAME_CONFIG['roles'])
-    player_socket.send(f'role:{player_role}')
+    player_socket.send(f'role:{player_role}'.encode())
 
     # wait for game to start
     while not has_game_started:
         sleep(0.1)
 
-    
-    option = random.choice(GAME_CONFIG['options'])
-    player_socket.send(json.)
     # serve option
+    option = random.choice(GAME_CONFIG['options'])
+    player_socket.send(json.dumps(option))
+    
     # wait for user choice
+    # {'player_choice' : 'scelta'}
+    msg = player_socket.recv(4096)
+    if not msg:
+        return
+    player_choice = json.loads(msg)
+
     # evaluate user choice
+    for choice in option['option_answers']:
+        if choice['option'] == player_choice and choice['correct'] == True:
+            break
+    else:
+        return
+    
     # while (number of questions < X):
-    #   serve question
-    #   wait for answer
-    #   check answer
-    #   assign points
+    while score < GAME_CONFIG['winning_score'] and not is_game_over:
+        # serve question
+        question = random.choice(GAME_CONFIG['questions'])
+        player_socket.send(json.dumps(question))
+
+        # wait for answer
+        msg = player_socket.recv(4096)
+        if not msg:
+            return
+
+        # slice answer out of message
+        # {'answer' : 3}
+        player_answer = json.loads(msg)['answer']
+        
+        # check answer and assign points
+        if player_answer == question['correct_answer']:
+            score += 1
+        else:
+            score -= 1 
+        
+        # send new score to client
+        score_update = {'score' : score}
+        player_socket.send(json.dumps(score_update))
+
+    # check if I won
+    if not is_game_over and score >= GAME_CONFIG['winning_score']:
+        is_game_over = True
+
+    # add my score to the ranking
+    ranking[player_name] = score
+    
+    # wait for all other threads to add their names
+    while len(ranking) < len(client_sockets):
+        sleep(0.1)
+    
+
+    # generate sorted ranking
+    sorted_ranking = dict(sorted(ranking.items(), reverse=True, key=lambda item: item[1]))
+
+    # serialize ranking
+    serialized_ranking = ""
+    for key in sorted_ranking:
+        serialized_ranking += f'{key}: {sorted_ranking[key]}'
+    
     # send ranking
+    player_socket.send(serialized_ranking)
+
     # close connection
+    player_socket.close()
+
     # exit
+    cancer_flag = True
 
-class ChatController():
-    def __init__(self, question_file_path):
-        self._game_data = DataParser(_game_data_file_path).get_game_data()
-        self._game = Game(self._game_data)
-        self._clients = {} # key:conn_id -> (client_socket, client_addr, Player)
-        self._next_connection_id = 0
-         
-    def start_game(self):
-        self._game.start_game()
+def start_server():
+    print('Ingegneria e Scienze Informatiche - UniBo')
+    print('Progetto Traccia #3 - Esame di Reti')
+    print('A. Conti, A. Mastrilli, U. Baroncini')
+    open_socket()
     
-    def stop_game(self):
-        self._game.stop_game()
+    while not cancer_flag:
+        sleep(0.1)
 
-    def is_game_running(self):
-        return self._game.is_game_running()
-    
-    def is_game_started(self):
 
-    def _get_option(self):
-        return self._game_data.options[0]
-
-    def _get_config(self, path):
-        with open(path, 'r') as file:
-            self._data = json.loads(file)
+start_server()
