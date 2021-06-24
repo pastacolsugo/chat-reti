a80de345b9e2066b8f70632f819f1fbe1876e37a
Ugo <ugobaroncini@gmail.com>
2021-06-24 04:10:39 +0200

    Worked on client

--- src/game_client.py
+++ src/game_client.py
@@ -9,10 +9,12 @@ import socket
 from time import sleep
 import threading
 
+from server import start_server
+
 # FINESTRA DI GIOCO PRINCIPALE
 window_main = tk.Tk()
 window_main.title("Game Client")
-your_name = ""
+player_name = ""
 opponent_name = ""
 game_round = 0
 game_timer = 4
@@ -26,170 +28,95 @@ opponent_score = 0
 client = None
 HOST_ADDR = '127.0.0.1'
 HOST_PORT = 8000
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST_ADDR, HOST_PORT))
-client.send("ali".encode())
-#load json file: his content is in the dictionary dic
-data = client.recv(4096)
-dic = json.loads(data.decode())
-#dictionary of options
-option_dictionary = dic['options']
-top_welcome_frame= tk.Frame(window_main)
-lbl_name = tk.Label(top_welcome_frame, text = "Name:")
-lbl_name.pack(side=tk.LEFT)
-ent_name = tk.Entry(top_welcome_frame)
-ent_name.pack(side=tk.LEFT)
-btn_connect = tk.Button(top_welcome_frame, text="Connect", command=lambda : connect())
-btn_connect.pack(side=tk.LEFT)
-top_welcome_frame.pack(side=tk.TOP)
 
-top_message_frame = tk.Frame(window_main)
-lbl_line = tk.Label(top_message_frame, text="***********************************************************").pack()
-lbl_welcome = tk.Label(top_message_frame, text="")
-lbl_welcome.pack()
-lbl_line_server = tk.Label(top_message_frame, text="***********************************************************")
-#lbl_line_server.pack_forget() #nascosto
-lbl_line_server.pack() #modificato per vedere qualcosa
-top_message_frame.pack(side=tk.TOP)
+# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client.connect((HOST_ADDR, HOST_PORT))
+# client.send("ali".encode())
 
+# load json file: his content is in the dictionary dic
+# data = client.recv(4096)
+# dic = json.loads(data.decode())
+# dictionary of options
+# option_dictionary = dic['options']
 
-top_frame = tk.Frame(window_main)
-top_left_frame = tk.Frame(top_frame, highlightbackground="green", highlightcolor="green", highlightthickness=1)
-lbl_your_name = tk.Label(top_left_frame, text="Your name: " + your_name, font = "Helvetica 13 bold")
-lbl_opponent_name = tk.Label(top_left_frame, text="Opponent: " + "Player")
-lbl_your_name.grid(row=0, column=0, padx=5, pady=8)
-lbl_opponent_name.grid(row=1, column=0, padx=5, pady=8)
-top_left_frame.pack(side=tk.LEFT, padx=(10, 10))
+player_name_frame = tk.Frame(window_main)
+name_label = tk.Label(player_name_frame, text = "Name:")
+name_label.pack(side = tk.LEFT)
+name_text_field = tk.Entry(player_name_frame)
+name_text_field.pack(side = tk.LEFT)
+connect_button = tk.Button(player_name_frame, text="Connect", command=lambda : connect())
+connect_button.pack(side = tk.LEFT)
+player_name_frame.pack(side = tk.TOP)
 
+score_and_role_frame = tk.Frame(window_main)
+score_and_role_frame.pack(side = tk.TOP)
 
-top_right_frame = tk.Frame(top_frame, highlightbackground="green", highlightcolor="green", highlightthickness=1)
-lbl_game_round = tk.Label(top_right_frame, text="Game round (x) starts in", foreground="blue", font = "Helvetica 14 bold")
-lbl_timer = tk.Label(top_right_frame, text=" ", font = "Helvetica 24 bold", foreground="blue")
-lbl_game_round.grid(row=0, column=0, padx=5, pady=5)
-lbl_timer.grid(row=1, column=0, padx=5, pady=5)
-top_right_frame.pack(side=tk.RIGHT, padx=(10, 10))
+score_display_frame = tk.Frame(score_and_role_frame)
+point_label = tk.Label(score_display_frame, text = "Score: 0")
+point_label.pack(side = tk.RIGHT)
+score_display_frame.pack(side = tk.RIGHT)
 
-#top_frame.pack_forget()
-top_frame.pack()
+role_display_frame = tk.Frame(score_and_role_frame)
+role_label = tk.Label(role_display_frame, text = "Role : -")
+role_label.pack(side = tk.LEFT)
+role_display_frame.pack(side = tk.LEFT)
 
-middle_frame = tk.Frame(window_main)
+question_display_frame = tk.Frame(window_main)
+question_label = tk.Label(question_display_frame, text = "Ready for some questions?")
+question_label.pack(side = tk.TOP)
+question_display_frame.pack(side = tk.TOP)
 
-lbl_line = tk.Label(middle_frame, text="***********************************************************").pack()
-lbl_line = tk.Label(middle_frame, text="**** GAME LOG ****", font = "Helvetica 13 bold", foreground="blue").pack()
-lbl_line = tk.Label(middle_frame, text="***********************************************************").pack()
+answer_buttons_frame = tk.Frame(window_main)
+answer_0_button = tk.Button(answer_buttons_frame, text='Answer 0', command = lambda : answer(0))
+answer_1_button = tk.Button(answer_buttons_frame, text='Answer 1', command = lambda : answer(1))
+answer_2_button = tk.Button(answer_buttons_frame, text='Answer 2', command = lambda : answer(2))
+answer_3_button = tk.Button(answer_buttons_frame, text='Answer 3', command = lambda : answer(3))
+answer_0_button.pack(side = tk.LEFT)
+answer_1_button.pack(side = tk.LEFT)
+answer_2_button.pack(side = tk.LEFT)
+answer_3_button.pack(side = tk.LEFT)
 
-round_frame = tk.Frame(middle_frame)
-#write in a new label, the question
-lbl_round = lab(round_frame, 'option_question')
-create_button(round_frame, 'option_answer', 'option', len(option_dictionary['option_answer']))
-#lbl_round = tk.Label(round_frame, text="Round")
-lbl_round.pack()
-lbl_your_choice = tk.Label(round_frame, text="Your choice: " + "None", font = "Helvetica 13 bold")
-lbl_your_choice.pack()
-lbl_opponent_choice = tk.Label(round_frame, text="Opponent choice: " + "None")
-lbl_opponent_choice.pack()
-lbl_result = tk.Label(round_frame, text=" ", foreground="blue", font = "Helvetica 14 bold")
-lbl_result.pack()
-round_frame.pack(side=tk.TOP)
+choice_buttons_frame = tk.Frame(window_main)
+choice_0_button = tk.Button(choice_buttons_frame, text = "Choice 0", command = lambda : choose(0))
+choice_1_button = tk.Button(choice_buttons_frame, text = "Choice 1", command = lambda : choose(1))
+choice_2_button = tk.Button(choice_buttons_frame, text = "Choice 2", command = lambda : choose(2))
+choice_0_button.pack(side = tk.LEFT)
+choice_1_button.pack(side = tk.LEFT)
+choice_2_button.pack(side = tk.LEFT)
 
-final_frame = tk.Frame(middle_frame)
-lbl_line = tk.Label(final_frame, text="***********************************************************").pack()
-lbl_final_result = tk.Label(final_frame, text=" ", font = "Helvetica 13 bold", foreground="blue")
-lbl_final_result.pack()
-lbl_line = tk.Label(final_frame, text="***********************************************************").pack()
-final_frame.pack(side=tk.TOP)
+start_button_frame = tk.Frame(window_main)
+start_button = tk.Button(start_button_frame, text="Start game!", command = lambda : send_start_game(), state=tk.DISABLED)
+start_button.pack(side = tk.TOP)
+start_button_frame.pack(side = tk.BOTTOM)
 
-#middle_frame.pack_forget()
-middle_frame.pack()
-button_frame = tk.Frame(window_main)
-#photo_rock = PhotoImage(file=r"rock.gif")
-#photo_paper = PhotoImage(file = r"paper.gif")
-#photo_scissors = PhotoImage(file = r"scissors.gif")
+def send_start_game():
+    pass
 
-btn_rock = tk.Button(button_frame, text="Rock", command=lambda : choice("rock"), state=tk.DISABLED)
-btn_paper = tk.Button(button_frame, text="Paper", command=lambda : choice("paper"), state=tk.DISABLED)
-btn_scissors = tk.Button(button_frame, text="Scissors", command=lambda : choice("scissors"), state=tk.DISABLED)
-btn_rock.grid(row=0, column=0)
-btn_paper.grid(row=0, column=1)
-btn_scissors.grid(row=0, column=2)
-button_frame.pack(side=tk.BOTTOM)
+def answer(ans_number):
+    print(f'Answered {ans_number}')
 
-#function that take create a list of answer
-#parameters: frame(the frame in which insert the buttons)
-#list: the list of answers
-def create_button(Frame, inner_dictionary, outer_dictionary, num_answers):
-    while num_answers != 0:
-        bt = tk.Button(Frame, text= data[inner_dictionary][num_answers][outer_dictionary])
-        num_answers = num_answers -1
+def choose(choice_number):
+    print(f'Chosen {choice_number}')
 
-def create_question_label(Frame, dictionary):
-    lab = tk.Label(Frame, text = option_dictionary[dictionary])
-    return lab
-def game_logic(you, opponent):
-    winner = ""
-    rock = "rock"
-    paper = "paper"
-    scissors = "scissors"
-    player0 = "you"
-    player1 = "opponent"
+def hide_answer_buttons():
+    answer_buttons_frame.pack_forget()
 
-    if you == opponent:
-        winner = "draw"
-    elif you == rock:
-        if opponent == paper:
-            winner = player1
-        else:
-            winner = player0
-    elif you == scissors:
-        if opponent == rock:
-            winner = player1
-        else:
-            winner = player0
-    elif you == paper:
-        if opponent == scissors:
-            winner = player1
-        else:
-            winner = player0
-    return winner
+def show_answer_buttons():
+    answer_buttons_frame.pack(side = tk.TOP)
 
+def hide_choice_buttons():
+    choice_buttons_frame.pack_forget()
 
-def enable_disable_buttons(todo):
-    if todo == "disable":
-        btn_rock.config(state=tk.DISABLED)
-        btn_paper.config(state=tk.DISABLED)
-        btn_scissors.config(state=tk.DISABLED)
-    else:
-        btn_rock.config(state=tk.NORMAL)
-        btn_paper.config(state=tk.NORMAL)
-        btn_scissors.config(state=tk.NORMAL)
-
+def show_choice_buttons():
+    choice_buttons_frame.pack(side = tk.TOP)
 
 def connect():
-    global your_name
-    if len(ent_name.get()) < 1:
+    global player_name
+    if len(name_text_field.get()) < 1:
         tk.messagebox.showerror(title="ERROR!!!", message="You MUST enter your first name <e.g. John>")
     else:
-        your_name = ent_name.get()
-        lbl_your_name["text"] = "Your name: " + your_name
-        connect_to_server(your_name)
-
-
-def count_down(my_timer, nothing):
-    global game_round
-    if game_round <= TOTAL_NO_OF_ROUNDS:
-        game_round = game_round + 1
-
-    lbl_game_round["text"] = "Game round " + str(game_round) + " starts in"
-
-    while my_timer > 0:
-        my_timer = my_timer - 1
-        print("game timer is: " + str(my_timer))
-        lbl_timer["text"] = my_timer
-        sleep(1)
-
-    enable_disable_buttons("enable")
-    lbl_round["text"] = "Round - " + str(game_round)
-    lbl_final_result["text"] = ""
+        player_name = name_text_field.get()
+        connect_to_server()
 
 
 def choice(arg):
@@ -202,40 +129,41 @@ def choice(arg):
         enable_disable_buttons("disable")
 
 
-def connect_to_server(name):
-    global client, HOST_PORT, HOST_ADDR, your_name
+def connect_to_server():
+    global client_socket, HOST_PORT, HOST_ADDR
     try:
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.connect((HOST_ADDR, HOST_PORT))
-        client.send(name.encode()) # Invia il nome al server dopo la connessione
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((HOST_ADDR, HOST_PORT))
+        client_socket.send(player_name.encode()) # Invia il nome al server dopo la connessione
 
         # disable widgets
-        btn_connect.config(state=tk.DISABLED)
-        ent_name.config(state=tk.DISABLED)
-        lbl_name.config(state=tk.DISABLED)
-        enable_disable_buttons("disable")
+        connect_button.config(state=tk.DISABLED)
+        name_text_field.config(state=tk.DISABLED)
+        name_label.config(state=tk.DISABLED)
+        start_button.config(state=tk.ACTIVE)
 
         # avvia un thread per continuare a ricevere messaggi dal server
         # non bloccare il thread principale :)
-        threading._start_new_thread(receive_message_from_server, (client, "m"))
+        threading._start_new_thread(receive_message_from_server, ())
     except Exception as e:
         tk.messagebox.showerror(title="ERROR!!!", message="Cannot connect to host: " + HOST_ADDR + " on port: " + str(HOST_PORT) + " Server may be Unavailable. Try again later")
+        print(e)
 
 
-def receive_message_from_server(sck, m):
-    global your_name, opponent_name, game_round
-    global your_choice, opponent_choice, your_score, opponent_score
+def receive_message_from_server():
+    global client_socket, player_name
 
     while True:
-        from_server = sck.recv(4096)
+        from_server = client_socket.recv(4096).decode()
 
-        if not from_server: break
+        if not from_server:
+            break
 
         if from_server.startswith("welcome".encode()):
             if from_server == "welcome1":
-                lbl_welcome["text"] = "Server says: Welcome " + your_name + "! Waiting for player 2"
+                lbl_welcome["text"] = "Server says: Welcome " + player_name + "! Waiting for player 2"
             elif from_server == "welcome2":
-                lbl_welcome["text"] = "Server says: Welcome " + your_name + "! Game will start soon"
+                lbl_welcome["text"] = "Server says: Welcome " + player_name + "! Game will start soon"
             lbl_line_server.pack()
 
         elif from_server.startswith("opponent_name$".encode()):
